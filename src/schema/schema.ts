0742ca30197709d6a3cb4dd3dbef82772d9e61c9
emp74ark <emp74ark@gmail.com>
2023-01-26 00:05:59 +0100

    feat: graphql lists

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,18 +1,21 @@
-import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLSchema } from 'graphql';
+import { GraphQLID, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 
 const movies = [
   {id: 1, name: 'One', genre: 'Crime', directorId: 1},
   {id: 2, name: 'Two', genre: 'Comedy', directorId: 2},
   {id: 3, name: 'Three', genre: 'Drama', directorId: 3},
-]
+  {id: 3, name: 'Four', genre: 'Drama', directorId: 1},
+  {id: 3, name: 'Five', genre: 'Drama', directorId: 1},
+  {id: 3, name: 'Six', genre: 'Drama', directorId: 2},
+];
 
 const directors = [
   {id: 1, name: 'Name One', age: 30},
   {id: 2, name: 'Name Two', age: 40},
   {id: 3, name: 'Name Three', age: 50},
-]
+];
 
-const MovieType = new GraphQLObjectType({
+const MovieType: GraphQLObjectType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
     id: {type: GraphQLID},
@@ -22,20 +25,26 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        return directors.find(el => el.id == parent.id)
+        return directors.find(el => el.id == parent.id);
       }
     }
   })
-})
+});
 
-const DirectorType = new GraphQLObjectType({
+const DirectorType: GraphQLObjectType = new GraphQLObjectType({
   name: 'Director',
   fields: () => ({
     id: {type: GraphQLID},
     name: {type: GraphQLString},
     age: {type: GraphQLInt},
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies.filter(el => el.directorId === parent.id)
+      }
+    }
   })
-})
+});
 
 const Query = new GraphQLObjectType({
   name: 'Query',
@@ -43,22 +52,36 @@ const Query = new GraphQLObjectType({
     movie: {
       type: MovieType,
       args: {id: {type: GraphQLID}},
-      resolve(parent, args){
-        return movies.find(el => el.id == args.id)
+      resolve(parent, args) {
+        return movies.find(el => el.id == args.id);
       }
     },
     director: {
       type: DirectorType,
       args: {id: {type: GraphQLID}},
-      resolve(parent, args){
-        return directors.find(el => el.id == args.id)
+      resolve(parent, args) {
+        return directors.find(el => el.id == args.id);
       }
-    }
+    },
+    movies: {
+      type: new GraphQLList(MovieType),
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args) {
+        return movies;
+      }
+    },
+    directors: {
+      type: new GraphQLList(DirectorType),
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args) {
+        return directors;
+      }
+    },
   }
-})
+});
 
 const movieSchema = new GraphQLSchema({
   query: Query,
-})
+});
 
-export {movieSchema}
+export { movieSchema };
