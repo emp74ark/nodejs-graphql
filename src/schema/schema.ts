27bd7f285390cba5951f64e855a8f3e2b63ae6b1
emp74ark <emp74ark@gmail.com>
2023-01-25 18:48:27 +0100

    feat: movies and directors relations

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,9 +1,15 @@
 import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLSchema } from 'graphql';
 
 const movies = [
-  {id: 1, name: 'One', genre: 'Crime'},
-  {id: 2, name: 'Two', genre: 'Comedy'},
-  {id: 3, name: 'Three', genre: 'Drama'},
+  {id: 1, name: 'One', genre: 'Crime', directorId: 1},
+  {id: 2, name: 'Two', genre: 'Comedy', directorId: 2},
+  {id: 3, name: 'Three', genre: 'Drama', directorId: 3},
+]
+
+const directors = [
+  {id: 1, name: 'Name One', age: 30},
+  {id: 2, name: 'Name Two', age: 40},
+  {id: 3, name: 'Name Three', age: 50},
 ]
 
 const MovieType = new GraphQLObjectType({
@@ -13,6 +19,21 @@ const MovieType = new GraphQLObjectType({
     name: {type: GraphQLString},
     genre: {type: GraphQLString},
     year: {type: GraphQLInt},
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find(el => el.id == parent.id)
+      }
+    }
+  })
+})
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt},
   })
 })
 
@@ -24,6 +45,13 @@ const Query = new GraphQLObjectType({
       args: {id: {type: GraphQLID}},
       resolve(parent, args){
         return movies.find(el => el.id == args.id)
+      }
+    },
+    director: {
+      type: DirectorType,
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args){
+        return directors.find(el => el.id == args.id)
       }
     }
   }
